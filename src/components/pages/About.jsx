7a3f25f78261afe9f1d96f21f98e4683fea56758
williamrankin <william.rankin01@gmail.com>
2024-03-30 10:29:30 -0600

    adjusted margin

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="container">
+    <div className="container mt-3">
       <div className="row justify-content-center">
         <div className="col-md-8">
           <div className="card mb-4">
